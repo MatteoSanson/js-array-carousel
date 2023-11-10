008752261ecb144c3c5f01a0870277fa10d9b813
MatteoSanson <matteo.sanson@gmail.com>
2023-11-10 17:52:43 +0100

    feat: milestone2

--- README.md
+++ README.md
@@ -8,5 +8,15 @@ Dato un array contenente una lista di cinque immagini, creare un carosello come 
 Rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
 Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 
+### Svolgimento
+1) Creo un array di immagini.
+2) Per ogni elemento immagine voglio inserire nel codice html un elemento `<div>`.
+3) Aggiungo la classe "attiva" al `<div>` sull'immagine corrente.
+
 ## MILESTONE 2
 Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+### Svolgimento
+1) genero la costante lista-nodi dei div presenti nel dom con la classe che mi interessa. 
+2) mi creo le variabili per i tasti prev e next.
+3) aggiungo evento al click dei tasti.
--- versione-base/js/main.js
+++ versione-base/js/main.js
@@ -17,6 +17,11 @@ const items = document.querySelector(`.items`);
 // variabile immagine che vedo in questo momento 
 let immagineCorrente = 0;
 
+// mlst2 2) mi creo le variabili per i tasti prev e next.
+const prev = document.querySelector(`.prev`);
+const next = document.querySelector(`.next`);
+
+// 2) Per ogni elemento immagine voglio inserirlo nel codice html.
 for (let i = 0; i < images.length; i++){
     // creo il mio item div con classe item
     const item = document.createElement(`div`);
@@ -33,3 +38,26 @@ for (let i = 0; i < images.length; i++){
     item.append(img);
     items.append(item);
 }
+
+// 1) genero la costante listanodi dei div presenti nel dom con la classe che mi interessa   
+const domItem = document.querySelectorAll(`.item`);
+console.log(domItem);
+
+// mlst2 3) aggiungo evento al click dei tasti. 
+prev.addEventListener (`click`, function(){
+    console.log(`ho cliccato sopra`);
+    if (immagineCorrente > 0) {
+        domItem[immagineCorrente].classList.remove(`active`);
+        immagineCorrente--;
+        domItem[immagineCorrente].classList.add(`active`);
+    }
+});
+
+next.addEventListener (`click`, function(){
+    console.log(`ho cliccato sotto`);
+    if (immagineCorrente < domItem.length - 1) {
+        domItem[immagineCorrente].classList.remove(`active`);
+        immagineCorrente++;
+        domItem[immagineCorrente].classList.add(`active`);
+    }
+});
